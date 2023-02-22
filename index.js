529083eaf4586c04c63123e00f403bbe6e37edd9
Aditya Vandan Sharma <35309453+AdityaVandan@users.noreply.github.com>
2023-02-22 11:51:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,71 @@
-const core = require('@actions/core');
-const wait = require('./wait');
+const npmCheck = require("npm-check");
 
+const DEPENDENCY = {
+  PRODUCTION: "PRODUCTION",
+  DEV: "DEV"
+};
 
-// most @actions toolkit packages have async methods
-async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+const STATUS = {
+  UP_TO_DATE: "UP_TO_DATE",
+  MAJOR: "MAJOR",
+  MINOR: "MINOR",
+  PATCH: "PATCH"
+};
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+const BUMP_TYPE = {
+  MAJOR: "major",
+  MINOR: "minor",
+  PATCH: "patch",
+  NON_SEMVER: "nonSemver"
+};
 
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
+const getStatus = packageObj => {
+  switch (packageObj.bump) {
+    case BUMP_TYPE.NON_SEMVER:
+    case BUMP_TYPE.MAJOR:
+      return STATUS.MAJOR;
+    case BUMP_TYPE.MINOR:
+      return STATUS.MINOR;
+    case BUMP_TYPE.PATCH:
+      return STATUS.PATCH;
+    case null:
+    case undefined:
+      return STATUS.UP_TO_DATE;
+    default:
+      return STATUS.UP_TO_DATE;
   }
-}
+};
 
-run();
+const getPackageList = options => {
+  npmCheck(options).then(currentState => {
+    const promisedPackageList = currentState.get("packages");
+    const packages = promisedPackageList.map(packageObj => ({
+      packageName: packageObj.moduleName,
+      packageUrl: packageObj.homepage,
+      installedVersion: packageObj.installed,
+      latestVersion: packageObj.latest,
+      status: getStatus(packageObj),
+      isUsed: !packageObj.unused,
+      type: packageObj.devDependency ? DEPENDENCY.DEV : DEPENDENCY.PRODUCTION
+    }));
+    console.log(packages.length);
+    console.log(packages);
+  });
+};
+
+const modulesToIgnore = [
+  "@app/..",
+  "@app/**",
+  "@hooks/**",
+  "@lib/**",
+  "@misc/**",
+  "@assets/**",
+  "@webapp/**",
+  "@hooks/**",
+  "@modules/**",
+  "@utilities/**"
+];
+
+getPackageList({
+  ignore: modulesToIgnore
+});
